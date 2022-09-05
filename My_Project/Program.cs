f20a7fa186c78b63ca9b46dd4335edc85bf0c514
Stanislav <stasvitalich1994@gmail.com>
2022-09-05 11:53:29 +0700

    добавили второй способ подсчёта квадрата числа

--- My_Project/Program.cs
+++ My_Project/Program.cs
@@ -6,3 +6,9 @@ int number = int.Parse(Console.ReadLine());
 //Получение квадрата входного числа
 int result = number * number;
 Console.WriteLine($"Квадрат чиcла {number} = {result}");
+
+//Значок $ позволяет вписывать числа в строки
+
+int result_2 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат чиcла {number} = {result}");
+
