4c77739534fb5ce411cfdb272265d4270e54b108
Stanislav <stasvitalich1994@gmail.com>
2022-09-05 11:43:47 +0700

    Была описана основа нашей программы

--- My_Project/Program.cs
+++ My_Project/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+//Определение входного числа
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+//Получение квадрата входного числа
+int result = number * number;
+Console.WriteLine($"Квадрат чиcла {number} = {result}");
