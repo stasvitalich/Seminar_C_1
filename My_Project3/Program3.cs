5f41b2c6e57b9073eb94266ae6812b102d47f04f
Stanislav <stasvitalich1994@gmail.com>
2022-09-05 13:57:50 +0700

    Код для дней недели был добавлен в программу

--- My_Project3/Program3.cs
+++ My_Project3/Program3.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (number == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (number == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (number == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (number == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (number == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (number == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+
