c3448b0e9037f4448a0ad51269270283641633b6
Stanislav <stasvitalich1994@gmail.com>
2022-09-05 13:30:42 +0700

    Записана вторая часть программы, отвечающая на вопрос да или нет

--- My_Project2/Program2.cs
+++ My_Project2/Program2.cs
@@ -1,4 +1,20 @@
-﻿Console.WriteLine("Введите первое число ");
+﻿//Ввод первоначальных данных
+Console.WriteLine("Введите первое число ");
 int number1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число ");
 int number2 = int.Parse(Console.ReadLine());
+
+//Записываем частное этих чисел в отдельную переменную
+int divide = number1 / number2;
+
+
+//Console.WriteLine($"Частное двух чисел равно {divide}");
+if (number2 == divide)
+{
+    Console.WriteLine("Да, первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Нет, первое число не является квадратом второго");
+}
+
