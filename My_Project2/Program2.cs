5e52cdbf105a042c4bedda648fcc7ec60d0ab7c0
Stanislav <stasvitalich1994@gmail.com>
2022-09-05 12:36:43 +0700

    Первая часть второй программы была добавлена

--- My_Project2/Program2.cs
+++ My_Project2/Program2.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine());
